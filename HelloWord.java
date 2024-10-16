8932f3dd6b819a01c79ca2ae12d37c5cc548e065
23jy0131 <23jy0131@jec.ac.jp>
2024-10-16 15:38:45 +0900

    日本電子専門学校の1行を追加

--- HelloWord.java
+++ HelloWord.java
@@ -1,1 +1,7 @@
-# my_new_repos
+public class HelloWorld {
+  public static void main(String[] args){
+    System.out.println("HelloWorld");
+    System.out.println("HelloWorld");
+    System.out.println("日本電子専門学校");
+  }
+}
